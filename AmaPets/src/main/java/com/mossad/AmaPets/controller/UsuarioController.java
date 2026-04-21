a3ce3f859d4dbd1dc3350685ce0fcb97a2c796b8
Felipe <Fel.fariass@duocuc.cl>
2026-04-21 09:39:51 -0400

    usuario casi listo

--- AmaPets/src/main/java/com/mossad/AmaPets/controller/UsuarioController.java
+++ AmaPets/src/main/java/com/mossad/AmaPets/controller/UsuarioController.java
@@ -1,5 +1,24 @@
 package com.mossad.AmaPets.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mossad.AmaPets.models.entities.Usuario;
+import com.mossad.AmaPets.service.UsuarioService;
+
+
+@RequestMapping("/usuarios")
+@RestController
+
 public class UsuarioController {
-    
+    @Autowired
+    private UsuarioService usuarioService;
+    @GetMapping("")
+    public List<Usuario> obtenerTodosLosUsuarios() {
+        return usuarioService.obtenerTodosLosUsuarios();
+    }
 }
--- AmaPets/src/main/java/com/mossad/AmaPets/service/UsuarioService.java
+++ AmaPets/src/main/java/com/mossad/AmaPets/service/UsuarioService.java
@@ -1,5 +1,19 @@
 package com.mossad.AmaPets.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mossad.AmaPets.models.entities.Usuario;
+import com.mossad.AmaPets.repository.UsuarioRepository;
+@Service
 public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usurepo;
+
+    public List<Usuario> obtenerTodosLosUsuarios() {
+        return usurepo.findAll();
+    }
     
 }
