534398063fbb5055027c641b49060789f2c91548
Felipe <Fel.fariass@duocuc.cl>
2026-04-22 09:05:05 -0400

    usuario

--- AmaPets/src/main/java/com/mossad/AmaPets/controller/MascotasController.java
+++
@@ -1,7 +0,0 @@
-package com.mossad.AmaPets.controller;
-
-import java.util.List;
-
-public class MascotasController {
-    
-}
--- AmaPets/src/main/java/com/mossad/AmaPets/repository/MascotasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mossad.AmaPets.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mossad.AmaPets.models.entities.Mascotas;
-
-public interface  MascotasRepository extends JpaRepository<Mascotas, Integer> {
-
-}
