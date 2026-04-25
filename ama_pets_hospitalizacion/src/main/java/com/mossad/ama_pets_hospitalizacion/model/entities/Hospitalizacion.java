5097745a554e55124d75a128c1d7fdeec2940135
Felipe <Fel.fariass@duocuc.cl>
2026-04-25 16:13:17 -0400

    MS Hospitalizacion

--- ama_pets_hospitalizacion/src/main/java/com/mossad/ama_pets_hospitalizacion/model/entities/Hospitalizacion.java
+++ ama_pets_hospitalizacion/src/main/java/com/mossad/ama_pets_hospitalizacion/model/entities/Hospitalizacion.java
@@ -1,4 +1,6 @@
 package com.mossad.ama_pets_hospitalizacion.model.entities;
+import java.util.Date;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,4 +17,14 @@ public class Hospitalizacion {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id_hospitalizacion;
 
+    @Column(nullable = false)
+    private Date fecha_ingreso;
+    @Column(nullable = false)
+    private Date fecha_egreso;
+    @Column(nullable = false)
+    private String observaciones;
+    @Column(nullable = false)
+    private int id_usuario;
+    @Column(nullable = false)
+    private int id_cirugia;
 }
--- ama_pets_hospitalizacion/src/main/java/com/mossad/ama_pets_hospitalizacion/service/HospitalizacionService.java
+++
@@ -1,5 +0,0 @@
-package com.mossad.ama_pets_hospitalizacion.service;
-
-public class HospitalizacionService {
-    
-}
