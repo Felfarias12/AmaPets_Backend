91e0b05dd840ba487597a3a96ab0ce34b78a2c18
Felipe <Fel.fariass@duocuc.cl>
2026-04-25 17:53:07 -0400

    se añaden puertos

---
+++ AmaPets_usuario/AmapetsUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.mossad.amapets_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmapetsUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmapetsUsuarioApplication.class, args);
+	}
+
+}
