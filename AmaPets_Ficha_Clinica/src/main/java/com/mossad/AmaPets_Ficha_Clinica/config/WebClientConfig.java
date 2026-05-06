8d2acca030f735f8dce9eb752c5eaf334b11b355
Mario Jaramillo <marioymegamanzx8@gmail.com>
2026-05-05 20:10:55 -0400

    conexion ficha clinica

---
+++ AmaPets_Ficha_Clinica/src/main/java/com/mossad/AmaPets_Ficha_Clinica/config/WebClientConfig.java
@@ -0,0 +1,36 @@
+package com.mossad.AmaPets_Ficha_Clinica.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    //veterinario
+    @Bean
+    public WebClient veterinarioWebClient(){
+        return WebClient.builder()
+        .baseUrl("http://localhost:7584/").build();
+    }
+
+     //tecnico medico
+     @Bean
+     public WebClient tecMedicoWebClient(){
+         return WebClient.builder()
+         .baseUrl("http://localhost:7581/").build();
+     }
+
+      //mascota
+      @Bean
+      public WebClient mascotaWebClient(){
+          return WebClient.builder()
+          .baseUrl("http://localhost:7579/").build();
+      }
+
+       //usuario
+       @Bean
+       public WebClient usuarioWebClient(){
+           return WebClient.builder()
+           .baseUrl("http://localhost:7580/").build();
+       }    
+}
